be1ec4380cdab8fd078f80510f5ce16cb60b8bdd
nagasjel <moutzebe@gmail.com>
2019-04-02 00:43:13 +0200

    Created plots problem 1a

--- code/problem1a.R
+++ code/problem1a.R
@@ -2,5 +2,13 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 rm(list=ls())
 library(tidyverse)
 library(MASS)
-data(seismic.dat)
-seismic = as.matrix(read.table('../data/seismic.dat'))
+library(reshape)
+seismic <- as.vector(read.table('../data/seismic.dat',row.names=NULL))
+seismic.df <- melt(vapply(seq(1,75),rep, rep(1,75), times= 75))
+seismic.df$value <- seismic$V1
+
+ggplot(seismic.df,aes(x= X1,y= X2)) + 
+  geom_tile(aes(fill=value))+
+  scale_fill_gradient(low ="white",
+                      high ="black")+
+  theme_minimal()
