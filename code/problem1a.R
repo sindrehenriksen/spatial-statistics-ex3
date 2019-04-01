88c17e6f4fb5c3302ea6c58c65e50048aab75532
nagasjel <moutzebe@gmail.com>
2019-04-02 00:45:23 +0200

    added save figure

--- code/problem1a.R
+++ code/problem1a.R
@@ -7,8 +7,12 @@ seismic <- as.vector(read.table('../data/seismic.dat',row.names=NULL))
 seismic.df <- melt(vapply(seq(1,75),rep, rep(1,75), times= 75))
 seismic.df$value <- seismic$V1
 
-ggplot(seismic.df,aes(x= X1,y= X2)) + 
+seismic.obs <- ggplot(seismic.df,aes(x= X1,y= X2)) + 
   geom_tile(aes(fill=value))+
   scale_fill_gradient(low ="white",
                       high ="black")+
   theme_minimal()
+
+ggsave("../figures/seismic_data.pdf", plot = seismic.obs, 
+       device = NULL, path = NULL, scale = 1, width = 5.5, 
+       height = 5.5, units = "in", dpi = 300, limitsize = TRUE)
